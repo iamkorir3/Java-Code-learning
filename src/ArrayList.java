cba073881b644221d056164ec9bec30895ac4c87
iamkorir3 <korir5956@gmail.com>
2025-10-16 23:57:28 +0300

    arraylist

---
+++ src/ArrayList.java
@@ -0,0 +1,17 @@
+public class ArrayList {
+
+    public void main() {
+        ArrayList<String> sal= new ArrayList<String>();
+
+        sal.add("hi");
+        sal.add("hello");
+        sal.add("hey");
+
+        int index = sal.indexOf("hello");
+
+        sal.contains("hey");
+
+
+    }
+    
+}
